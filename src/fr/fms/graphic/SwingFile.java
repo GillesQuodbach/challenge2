06344c5065a569e7ac11d554354982d90a29aec0
Gilles <quodbach.gilles@gmail.com>
2024-01-25 10:55:53 +0100

    implémentation chrono

--- src/fr/fms/graphic/SwingFile.java
+++ src/fr/fms/graphic/SwingFile.java
@@ -9,32 +9,46 @@ package fr.fms.graphic;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class SwingFile extends JFrame implements Runnable{
 	private static final long serialVersionUID = 1L;
 	private static final String TITLE = "Notre super app";
 	
 	JLabel clockLabel;
+	JLabel chronoLabel;
 	
 	public SwingFile() {
 		super(TITLE);			
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(500, 500);		
-		setLocationRelativeTo(null);	
+		setLocationRelativeTo(null);
+		JPanel panel = (JPanel) this.getContentPane();
+		panel.setLayout(new BorderLayout());
 		
+		//HORLOGE
 		clockLabel = new JLabel();
+		clockLabel.setFont(new Font("Serif", Font.BOLD,30));
+//		clockLabel.setForeground(Color.RED);
+//		clockLabel.setBackground(Color.ORANGE);
+//		clockLabel.setOpaque(true);
+		this.add(clockLabel, BorderLayout.NORTH);
 		
-		this.add(clockLabel);
-		
-		this.setLayout(new FlowLayout());
+		//CHRONO
+		chrono();
 		
 		// Horloge
 		Thread thread = new Thread(this);
@@ -42,6 +56,29 @@ public class SwingFile extends JFrame implements Runnable{
 		
 		
 		setVisible(true);	
+	}
+	
+	public void chrono() {
+		chronoLabel = new JLabel();
+		chronoLabel.setFont(new Font("Serif", Font.BOLD,30));
+		
+		this.add(chronoLabel);
+		
+		JButton chronoBtn = (new JButton("CLIC"));
+		chronoBtn.setPreferredSize(new Dimension(100,50));
+		this.add(chronoBtn, BorderLayout.SOUTH);
+		
+		
+//		DateFormat df = new SimpleDateFormat("HH:mm:ss");
+//		while (true) {
+//			try {
+//			String clock =df.format(new Date());
+//			clockLabel.setText(clock);
+//			
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
 	}
 		
 		public void run() {
